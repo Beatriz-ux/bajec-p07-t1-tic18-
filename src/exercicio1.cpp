26f085502510e2849d4af675b917b12f78610155
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-09-06 20:12:39 -0300

    feat: exercicio1

--- src/exercicio1.cpp
+++ src/exercicio1.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+    int i;
+
+    // a)
+    for(i=1; i<=100; i++){
+        if(i % 3 == 0)
+            cout << "Fizz";
+        if(i % 5 == 0)
+            cout << "Buzz";
+        if(!(i % 3 == 0) && !(i % 5 == 0))
+            cout << i;
+        cout << endl;
+    }
+
+    return 0;
+}
