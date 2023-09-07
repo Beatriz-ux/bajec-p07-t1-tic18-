279eac3df8d3e55174216444a6fa68959b5ae612
Caua-Clemente <cauaclementelima@gmail.com>
2023-09-07 20:38:27 -0300

    feat: Adicionando exercicio2

--- src/exercicio1.cpp
+++ src/exercicio1.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
 
+int main()
+{
+    for(int i = 1; i <= 100; i++)
+    {
+        std::cout << i; 
+        std::cout << (i % 3 == 0 ? "Fizz" : "");
+        std::cout << (i % 5 == 0 ? "Buzz" : "");
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
